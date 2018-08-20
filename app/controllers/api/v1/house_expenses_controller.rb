63788d617b2c4d86eee13a156fca32b97ac03a70
GCdM <gabrielcm.br@gmail.com>
2018-08-20 14:10:36 +0100

    Minor changes

---
+++ app/controllers/api/v1/house_expenses_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::HouseExpensesController < ApplicationController
+  before_action :set_household
+
+  def index
+    @expenses = @household.expenses
+
+    render json: @expenses
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+  end
+
+  private
+
+  def set_household
+    @household = Household.find(params[:household_id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:user_id, :date, :purchase, :description, :amount)
+  end
+end
