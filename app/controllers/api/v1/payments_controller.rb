831e5a719fc368168b3eaca60f0dbfe05356ee9b
github username <github email address>
2018-08-21 17:37:11 +0100

    final

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::PaymentsController < ApplicationController
-  before_action :set_expense
 
   def index
+    set_expense
     @payments = @expense.payments
 
     render json: @payments, each_serializer: PaymentsSerializer
   end
 
+  def update
+    @payment = Payment.find(params[:id])
+    @payment.update( paid: !@payment.paid)
+
+    render json: @payment, each_serializer: PaymentsSerializer
+  end
   private
 
   def set_expense
